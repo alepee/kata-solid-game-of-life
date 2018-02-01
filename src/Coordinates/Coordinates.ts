7ad6feb2aeb9622fd4861801f81d34c75a0944d0
Antoine Lépée <alepee@gmail.com>
2018-02-01 16:12:55 +0100

    feat: implement Coordinates

--- src/Coordinates/Coordinates.ts
+++ src/Coordinates/Coordinates.ts
@@ -1,6 +1,20 @@
+import CoordinateNumberXAxis from '../CoordinateNumber/CoordinateNumberXAxis';
+import CoordinateNumberYAxis from '../CoordinateNumber/CoordinateNumberYAxis';
+
 class Coordinates {
-    isNeighbour(coords: Coordinates): boolean {
-        return true;
+    private coordX: CoordinateNumberXAxis;
+    private coordY: CoordinateNumberYAxis;
+
+    constructor(coordX: CoordinateNumberXAxis, coordY: CoordinateNumberYAxis) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    isNeighbourOf(coords: Coordinates): boolean {
+        const x = this.coordX.distance(coords.coordX);
+        const y = this.coordY.distance(coords.coordY);
+
+        return [0, 1].includes(x) && [0, 1].includes(y) && x + y !== 0;
     }
 }
 
--- tests/Coordinates.test.ts
+++ tests/Coordinates.test.ts
@@ -3,16 +3,42 @@ import CoordinateNumberXAxis from '../src/CoordinateNumber/CoordinateNumberXAxis
 import CoordinateNumberYAxis from '../src/CoordinateNumber/CoordinateNumberYAxis';
 
 describe('Coordinates', () => {
-    it('will consider two adjacent Coordinates as neighbours', () => {
-        const coordsA = Coordinates.buildFromCoordinateNumbers(
+    it('will not consider two distant Coordinates', () => {
+        const coordsA = new Coordinates(
             new CoordinateNumberXAxis(3),
             new CoordinateNumberYAxis(3)
         );
-        const coordsB = Coordinates.buildFromCoordinateNumbers(
+        const coordsB = new Coordinates(
             new CoordinateNumberXAxis(4),
             new CoordinateNumberYAxis(9)
         );
 
-        expect(coordsA.isNeighbour(coordsB)).toBe(false);
+        expect(coordsA.isNeighbourOf(coordsB)).toBe(false);
+    });
+
+    it('will consider two adjacent Coordinates as neighbours', () => {
+        const coordsA = new Coordinates(
+            new CoordinateNumberXAxis(2),
+            new CoordinateNumberYAxis(3)
+        );
+        const coordsB = new Coordinates(
+            new CoordinateNumberXAxis(3),
+            new CoordinateNumberYAxis(3)
+        );
+
+        expect(coordsA.isNeighbourOf(coordsB)).toBe(true);
+    });
+
+    it('will not consider two identical Coordinates as neighbours', () => {
+        const coordsA = new Coordinates(
+            new CoordinateNumberXAxis(3),
+            new CoordinateNumberYAxis(3)
+        );
+        const coordsB = new Coordinates(
+            new CoordinateNumberXAxis(3),
+            new CoordinateNumberYAxis(3)
+        );
+
+        expect(coordsA.isNeighbourOf(coordsB)).toBe(false);
     });
 });
